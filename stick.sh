ab11adbe1b4e2b6748a359f8ae6c93f8f7a58871
RJ Bergeron <rbergero@gmail.com>
2016-02-15 21:01:34 -0500

    taught it how to EFI (via grub, so now I have two bootloaders...)

--- stick.sh
+++ stick.sh
@@ -22,6 +22,22 @@ curl -L -o vmlinuz http://mirrors.kernel.org/centos/7/os/x86_64/isolinux/vmlinuz
 echo 'retrieving initrd'
 curl -L -o initrd.img http://mirrors.kernel.org/centos/7/os/x86_64/isolinux/initrd.img
 
+echo 'retrieving EFI kit'
+curl -L -o unicode.pf2 http://mirrors.kernel.org/centos/7/os/x86_64/EFI/BOOT/fonts/unicode.pf2
+curl -L -o grubx64.efi http://mirrors.kernel.org/centos/7/os/x86_64/EFI/BOOT/grubx64.efi
+curl -L -o MokManager.efi http://mirrors.kernel.org/centos/7/os/x86_64/EFI/BOOT/MokManager.efi
+curl -L -o BOOTX64.EFI http://mirrors.kernel.org/fedora/releases/23/Workstation/x86_64/os/EFI/BOOT/BOOTX64.EFI
+
+echo 'copying EFI imagery'
+sudo env MTOOLS_SKIP_CHECK=1 mmd -i ${TARGET}1 EFI
+sudo env MTOOLS_SKIP_CHECK=1 mmd -i ${TARGET}1 EFI/BOOT
+sudo env MTOOLS_SKIP_CHECK=1 mmd -i ${TARGET}1 EFI/BOOT/fonts
+sudo env MTOOLS_SKIP_CHECK=1 mcopy -i ${TARGET}1 unicode.pf2 ::EFI/BOOT/fonts
+sudo env MTOOLS_SKIP_CHECK=1 mcopy -i ${TARGET}1 grubx64.efi ::EFI/BOOT
+sudo env MTOOLS_SKIP_CHECK=1 mcopy -i ${TARGET}1 grub.cfg ::EFI/BOOT
+sudo env MTOOLS_SKIP_CHECK=1 mcopy -i ${TARGET}1 MokManager.efi ::EFI/BOOT
+sudo env MTOOLS_SKIP_CHECK=1 mcopy -i ${TARGET}1 BOOTX64.EFI ::EFI/BOOT
+
 echo 'copying vmlinuz, initrd to usb stick'
 sudo env MTOOLS_SKIP_CHECK=1 mcopy -i ${TARGET}1 vmlinuz ::
 sudo env MTOOLS_SKIP_CHECK=1 mcopy -i ${TARGET}1 initrd.img ::
